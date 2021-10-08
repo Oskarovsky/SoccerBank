5737e23349fb6dc7521d0eb9e84b0306011f3947
Oskarovsky <oskar.slyk@gmail.com>
2021-10-08 19:17:07 +0200

    Init

---
+++ src/main/java/com/oskarro/soccerbank/SoccerBankApplication.java
@@ -0,0 +1,13 @@
+package com.oskarro.soccerbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oskarro/soccerbank/SoccerBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskarro.soccerbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
